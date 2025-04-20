e55d98ef2fe0766be2bbd9143ad9bbe45a731244
krfaima <132078459+krfaima@users.noreply.github.com>
2025-04-20 18:59:02 +0200

    Update api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
+import 'package:appone/Parking.dart';
 import 'package:http/http.dart' as http;
-import 'parking.dart'; // le fichier modèle
 
 Future<List<Parking>> fetchParkings() async {
   // final response = await http.get(Uri.parse('http://192.168.100.6:8000/api/parkings/')); // utilise l’IP selon ton environnement
